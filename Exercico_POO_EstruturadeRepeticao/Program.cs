5c7f564c21edd7c253e8e6df409b2405ebd53314
rafael chaves <rafaelaugustochaves@hotmail.com>
2022-02-14 18:52:58 -0300

    Adicionar arquivos de projeto.

---
+++ Exercico_POO_EstruturadeRepeticao/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercico_POO_EstruturadeRepeticao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
